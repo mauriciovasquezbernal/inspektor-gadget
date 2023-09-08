b7b37c7344013cd4bdf2624f182883515bf6b030
Mauricio Vásquez <mauriciov@microsoft.com>
2023-09-08 13:23:21 -0500

    Create include folder with all header files exposed by IG
    Consolidate all headers files that are used by gadgets in the same place.
    This commit also provides a make file to install them in the system.
    
    TODO:
    - Remove same files from other locations
      - pkg/arm64
      - pkg/amd64
      - pkg/gadgets/common,
      - pkg/gadgets/internal/socketenricher/bpf
    - Update all gadgets to use these
    - Another commits:
      - Provide release artifact with them?
      - Update packages to include and install them? (Should it be a -dev package?)

---
+++ include/gadget/types.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef __TYPES_H
+#define __TYPES_H
+
+// Keep these types aligned with definitions in pkg/gadgets/run/tracer/tracer.go.
+
+// union defining either an IPv4 or IPv6 address
+union gadget_ip_addr_t {
+	__u8 v6[16];
+	__u32 v4;
+};
+
+// struct defining either an IPv4 or IPv6 L3 endpoint
+struct gadget_l3endpoint_t {
+	union gadget_ip_addr_t addr;
+	__u8 version; // 4 or 6
+	__u8 pad[3]; // manual padding to avoid issues between C and Go
+};
+
+// struct defining an L4 endpoint
+struct gadget_l4endpoint_t {
+	struct gadget_l3endpoint_t l3;
+	__u16 port;
+	__u16 proto; // IP protocol number
+};
+
+// Inode id of a mount namespace. It's used to enrich the event in user space
+typedef u64 mnt_ns_id_t;
+
+#endif /* __TYPES_H */
