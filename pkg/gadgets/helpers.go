mauriciovasquezbernal/inspektor-gadget

commit f874869472739783ecc5e87478fe2c0173f29f4b

Rewrite detectBpfKtimeGetBootNs using sync.Once

pkg/gadgets/helpers.go
@@ -17,6 +17,7 @@ package gadgets
 import (
 	"encoding/binary"
 	"net/netip"
+	"sync"
 	"time"
 	"unsafe"
 
@@ -146,22 +147,32 @@ func WallTimeFromBootTime(ts uint64) types.Time {
 	return types.Time(time.Unix(0, int64(ts)).Add(timeDiff).UnixNano())
 }
 
-// detectBpfKtimeGetBootNs returns true if bpf_ktime_get_boot_ns is available
+var (
+	bpfKtimeGetBootNsOnce   sync.Once
+	bpfKtimeGetBootNsExists bool
+)
+
+// DetectBpfKtimeGetBootNs returns true if bpf_ktime_get_boot_ns is available
 // in the current kernel. False negatives are possible if BTF is not available.
-func detectBpfKtimeGetBootNs() bool {
-	btfSpec, err := btf.LoadKernelSpec()
-	if err != nil {
-		return false
-	}
+func DetectBpfKtimeGetBootNs() bool {
+	bpfKtimeGetBootNsOnce.Do(func() {
+		btfSpec, err := btf.LoadKernelSpec()
+		if err != nil {
+			bpfKtimeGetBootNsExists = false
+			return
+		}
 
-	enum := &btf.Enum{}
+		enum := &btf.Enum{}
+		err = btfSpec.TypeByName("bpf_func_id", &enum)
+		if err != nil {
+			bpfKtimeGetBootNsExists = false
+			return
+		}
 
-	err = btfSpec.TypeByName("bpf_func_id", &enum)
-	if err != nil {
-		return false
-	}
+		bpfKtimeGetBootNsExists = len(enum.Values) >= BpfKtimeGetBootNsFuncID
+	})
 
-	return len(enum.Values) >= BpfKtimeGetBootNsFuncID
+	return bpfKtimeGetBootNsExists
 }
 
 // removeBpfKtimeGetBootNs removes calls to bpf_ktime_get_boot_ns and replaces
@@ -186,7 +197,7 @@ func removeBpfKtimeGetBootNs(p *ebpf.ProgramSpec) {
 // FixBpfKtimeGetBootNs checks if bpf_ktime_get_boot_ns is supported by the
 // kernel and removes it if not
 func FixBpfKtimeGetBootNs(programSpecs map[string]*ebpf.ProgramSpec) {
-	if detectBpfKtimeGetBootNs() {
+	if DetectBpfKtimeGetBootNs() {
 		return
 	}
 

commit fa1bb9a358811570116e3df9d62f085ed14bebc1

pkg/helpers: Fix logic to detect if kernel supports bpf_ktime_get_boot_ns().
The kernel defines many BPF helpers functions and all of them have an ID [1].
Sadly, we cannot compare the number of helper functions with the ID to know if
the kernel supports the given function.
Indeed, the kernel defines two abstract helper functions:
* unspec which has ID 0 [2]
* and __BPF_FUNC_MAX_ID which indicates the last ID [3].

The solution is then to test that latest helper is __BPF_FUNC_MAX_ID and that
bpf_ktime_get_boot_ns() ID is lower.

Fixes: f87486947273 ("Rewrite detectBpfKtimeGetBootNs using sync.Once")
Fixes: ffb7d9910f2d ("gadgets: get timestamp from userspace on Linux <5.7")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>
[1]: https://github.com/torvalds/linux/blob/0e945134b680040b8613e962f586d91b6d40292d/include/uapi/linux/bpf.h#L5646
[2]: https://github.com/torvalds/linux/blob/0e945134b680040b8613e962f586d91b6d40292d/include/uapi/linux/bpf.h#L5647
[3]: https://github.com/torvalds/linux/blob/0e945134b680040b8613e962f586d91b6d40292d/include/uapi/linux/bpf.h#L5873

pkg/gadgets/helpers.go
@@ -209,7 +209,12 @@ func DetectBpfKtimeGetBootNs() bool {
 			return
 		}
 
-		bpfKtimeGetBootNsExists = len(enum.Values) >= BpfKtimeGetBootNsFuncID
+		last := enum.Values[len(enum.Values) - 1]
+		if last.Name != "__BPF_FUNC_MAX_ID" {
+			panic("Last BPF helper must be __BPF_FUNC_MAX_ID")
+		}
+
+		bpfKtimeGetBootNsExists = last.Value > BpfKtimeGetBootNsFuncID
 	})
 
 	return bpfKtimeGetBootNsExists