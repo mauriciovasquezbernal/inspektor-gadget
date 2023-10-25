mauriciovasquezbernal/inspektor-gadget

commit 65143e8ce269824a5aeca76946f73557c70ce75d

Decreasing the size of trace/open and trace/exec hash maps
Signed-off-by: Ben <ben@armosec.io>

Adding object files

Signed-off-by: Ben <ben@armosec.io>

pkg/gadgets/trace/exec/tracer/bpf/execsnoop.bpf.c
@@ -20,7 +20,7 @@ static const struct event empty_event = {};
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 10240);
+	__uint(max_entries, 1024);
 	__type(key, pid_t);
 	__type(value, struct event);
 } execs SEC(".maps");

pkg/gadgets/trace/open/tracer/bpf/opensnoop.bpf.c
@@ -22,7 +22,7 @@ const struct event *unusedevent __attribute__((unused));
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 10240);
+	__uint(max_entries, 1024);
 	__type(key, u32);
 	__type(value, struct event);
 } start SEC(".maps");

commit 5af70a3e0745fb0939f18c37285a2c3a022e0b70

trace {open, exec}: Revert map sizes to their previous value.
Having only 1024 entries impedes vertical scaling.
Thus, when the memory hungry are not used (i.e. gadgets run without --full-path
and --cwd), we use 10240 entries.
When these options are used, we stick with 1024 entries.

Fixes: 65143e8ce269 ("Decreasing the size of trace/open and trace/exec hash maps")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

pkg/gadgets/trace/exec/tracer/bpf/execsnoop.bpf.c
@@ -20,7 +20,11 @@ static const struct event empty_event = {};
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
+#ifdef WITH_CWD
 	__uint(max_entries, 1024);
+#else /* !WITH_CWD */
+	__uint(max_entries, 10240);
+#endif /* !WITH_CWD */
 	__type(key, pid_t);
 	__type(value, struct event);
 } execs SEC(".maps");

pkg/gadgets/trace/open/tracer/bpf/opensnoop.bpf.c
@@ -16,7 +16,6 @@ const volatile pid_t targ_pid = 0;
 const volatile pid_t targ_tgid = 0;
 const volatile uid_t targ_uid = INVALID_UID;
 const volatile bool targ_failed = false;
-const volatile bool get_full_path = false;
 const volatile __u32 prefixes_nr = 0;
 
 // we need this to make sure the compiler doesn't remove our struct
@@ -24,7 +23,11 @@ const struct event *unusedevent __attribute__((unused));
 
 struct {
 	__uint(type, BPF_MAP_TYPE_HASH);
+#ifdef WITH_FULL_PATH
 	__uint(max_entries, 1024);
+#else /* !WITH_FULL_PATH */
+	__uint(max_entries, 10240);
+#endif /* !WITH_FULL_PATH */
 	__type(key, u32);
 	__type(value, struct event);
 } start SEC(".maps");
@@ -175,8 +178,9 @@ static __always_inline int trace_exit(struct trace_event_raw_sys_exit *ctx)
 	u32 pid = bpf_get_current_pid_tgid();
 	u64 uid_gid = bpf_get_current_uid_gid();
 	u64 mntns_id;
+#ifdef WITH_FULL_PATH
 	size_t full_fname_len = 0;
-
+#endif /* WITH_FULL_PATH */
 	event = bpf_map_lookup_elem(&start, &pid);
 	if (!event)
 		return 0; /* missed entry */
@@ -194,8 +198,9 @@ static __always_inline int trace_exit(struct trace_event_raw_sys_exit *ctx)
 	event->mntns_id = gadget_get_mntns_id();
 	event->timestamp = bpf_ktime_get_boot_ns();
 
+#ifdef WITH_FULL_PATH
 	// Attempting to extract the full file path with symlink resolution
-	if (ret >= 0 && get_full_path) {
+	if (ret >= 0) {
 		long r = read_full_path_of_open_file_fd(
 			ret, (char *)event->full_fname,
 			sizeof(event->full_fname));
@@ -211,12 +216,17 @@ static __always_inline int trace_exit(struct trace_event_raw_sys_exit *ctx)
 		event->full_fname[0] = '\0';
 		full_fname_len = 1;
 	}
+#endif /* WITH_FULL_PATH */
 
 	/* emit event */
+#ifdef WITH_FULL_PATH
 	bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, event,
 			      sizeof(struct event) -
 				      (PATH_MAX - full_fname_len));
-
+#else /* !WITH_FULL_PATH */
+	bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, event,
+			      sizeof(struct event));
+#endif /* !WITH_FULL_PATH */
 cleanup:
 	bpf_map_delete_elem(&start, &pid);
 	return 0;

pkg/gadgets/trace/open/tracer/bpf/opensnoop.h
@@ -19,8 +19,10 @@ struct event {
 	__u16 mode;
 	__u8 comm[TASK_COMM_LEN];
 	__u8 fname[NAME_MAX];
+#ifdef WITH_FULL_PATH
 	// Keep full_fname as the last field for optimization
 	__u8 full_fname[PATH_MAX];
+#endif /* WITH_FULL_PATH */
 };
 
 struct prefix_key {

pkg/gadgets/trace/open/tracer/opensnoop_bpfel.go
@@ -24,7 +24,6 @@ type opensnoopEvent struct {
 	Mode      uint16
 	Comm      [16]uint8
 	Fname     [255]uint8
-	FullFname [4096]uint8
 	_         [7]byte
 }
 

pkg/gadgets/trace/open/tracer/opensnoopwithfullpath_bpfel.go
@@ -1,0 +1,165 @@
+// Code generated by bpf2go; DO NOT EDIT.
+//go:build 386 || amd64 || amd64p32 || arm || arm64 || loong64 || mips64le || mips64p32le || mipsle || ppc64le || riscv64
+
+package tracer
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"io"
+
+	"github.com/cilium/ebpf"
+)
+
+type opensnoopWithFullPathEvent struct {
+	Timestamp uint64
+	Pid       uint32
+	Uid       uint32
+	Gid       uint32
+	_         [4]byte
+	MntnsId   uint64
+	Ret       int32
+	Flags     int32
+	Mode      uint16
+	Comm      [16]uint8
+	Fname     [255]uint8
+	FullFname [4096]uint8
+	_         [7]byte
+}
+
+type opensnoopWithFullPathPrefixKey struct {
+	Prefixlen uint32
+	Filename  [255]uint8
+	_         [1]byte
+}
+
+// loadOpensnoopWithFullPath returns the embedded CollectionSpec for opensnoopWithFullPath.
+func loadOpensnoopWithFullPath() (*ebpf.CollectionSpec, error) {
+	reader := bytes.NewReader(_OpensnoopWithFullPathBytes)
+	spec, err := ebpf.LoadCollectionSpecFromReader(reader)
+	if err != nil {
+		return nil, fmt.Errorf("can't load opensnoopWithFullPath: %w", err)
+	}
+
+	return spec, err
+}
+
+// loadOpensnoopWithFullPathObjects loads opensnoopWithFullPath and converts it into a struct.
+//
+// The following types are suitable as obj argument:
+//
+//	*opensnoopWithFullPathObjects
+//	*opensnoopWithFullPathPrograms
+//	*opensnoopWithFullPathMaps
+//
+// See ebpf.CollectionSpec.LoadAndAssign documentation for details.
+func loadOpensnoopWithFullPathObjects(obj interface{}, opts *ebpf.CollectionOptions) error {
+	spec, err := loadOpensnoopWithFullPath()
+	if err != nil {
+		return err
+	}
+
+	return spec.LoadAndAssign(obj, opts)
+}
+
+// opensnoopWithFullPathSpecs contains maps and programs before they are loaded into the kernel.
+//
+// It can be passed ebpf.CollectionSpec.Assign.
+type opensnoopWithFullPathSpecs struct {
+	opensnoopWithFullPathProgramSpecs
+	opensnoopWithFullPathMapSpecs
+}
+
+// opensnoopWithFullPathSpecs contains programs before they are loaded into the kernel.
+//
+// It can be passed ebpf.CollectionSpec.Assign.
+type opensnoopWithFullPathProgramSpecs struct {
+	IgOpenE   *ebpf.ProgramSpec `ebpf:"ig_open_e"`
+	IgOpenX   *ebpf.ProgramSpec `ebpf:"ig_open_x"`
+	IgOpenatE *ebpf.ProgramSpec `ebpf:"ig_openat_e"`
+	IgOpenatX *ebpf.ProgramSpec `ebpf:"ig_openat_x"`
+}
+
+// opensnoopWithFullPathMapSpecs contains maps before they are loaded into the kernel.
+//
+// It can be passed ebpf.CollectionSpec.Assign.
+type opensnoopWithFullPathMapSpecs struct {
+	Bufs                 *ebpf.MapSpec `ebpf:"bufs"`
+	Events               *ebpf.MapSpec `ebpf:"events"`
+	GadgetMntnsFilterMap *ebpf.MapSpec `ebpf:"gadget_mntns_filter_map"`
+	PrefixKeys           *ebpf.MapSpec `ebpf:"prefix_keys"`
+	Prefixes             *ebpf.MapSpec `ebpf:"prefixes"`
+	Start                *ebpf.MapSpec `ebpf:"start"`
+}
+
+// opensnoopWithFullPathObjects contains all objects after they have been loaded into the kernel.
+//
+// It can be passed to loadOpensnoopWithFullPathObjects or ebpf.CollectionSpec.LoadAndAssign.
+type opensnoopWithFullPathObjects struct {
+	opensnoopWithFullPathPrograms
+	opensnoopWithFullPathMaps
+}
+
+func (o *opensnoopWithFullPathObjects) Close() error {
+	return _OpensnoopWithFullPathClose(
+		&o.opensnoopWithFullPathPrograms,
+		&o.opensnoopWithFullPathMaps,
+	)
+}
+
+// opensnoopWithFullPathMaps contains all maps after they have been loaded into the kernel.
+//
+// It can be passed to loadOpensnoopWithFullPathObjects or ebpf.CollectionSpec.LoadAndAssign.
+type opensnoopWithFullPathMaps struct {
+	Bufs                 *ebpf.Map `ebpf:"bufs"`
+	Events               *ebpf.Map `ebpf:"events"`
+	GadgetMntnsFilterMap *ebpf.Map `ebpf:"gadget_mntns_filter_map"`
+	PrefixKeys           *ebpf.Map `ebpf:"prefix_keys"`
+	Prefixes             *ebpf.Map `ebpf:"prefixes"`
+	Start                *ebpf.Map `ebpf:"start"`
+}
+
+func (m *opensnoopWithFullPathMaps) Close() error {
+	return _OpensnoopWithFullPathClose(
+		m.Bufs,
+		m.Events,
+		m.GadgetMntnsFilterMap,
+		m.PrefixKeys,
+		m.Prefixes,
+		m.Start,
+	)
+}
+
+// opensnoopWithFullPathPrograms contains all programs after they have been loaded into the kernel.
+//
+// It can be passed to loadOpensnoopWithFullPathObjects or ebpf.CollectionSpec.LoadAndAssign.
+type opensnoopWithFullPathPrograms struct {
+	IgOpenE   *ebpf.Program `ebpf:"ig_open_e"`
+	IgOpenX   *ebpf.Program `ebpf:"ig_open_x"`
+	IgOpenatE *ebpf.Program `ebpf:"ig_openat_e"`
+	IgOpenatX *ebpf.Program `ebpf:"ig_openat_x"`
+}
+
+func (p *opensnoopWithFullPathPrograms) Close() error {
+	return _OpensnoopWithFullPathClose(
+		p.IgOpenE,
+		p.IgOpenX,
+		p.IgOpenatE,
+		p.IgOpenatX,
+	)
+}
+
+func _OpensnoopWithFullPathClose(closers ...io.Closer) error {
+	for _, closer := range closers {
+		if err := closer.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Do not access this directly.
+//
+//go:embed opensnoopwithfullpath_bpfel.o
+var _OpensnoopWithFullPathBytes []byte

pkg/gadgets/trace/open/tracer/tracer.go
@@ -35,6 +35,7 @@ import (
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -no-global-types -target bpfel -cc clang -cflags ${CFLAGS} -type event -type prefix_key opensnoop ./bpf/opensnoop.bpf.c -- -I./bpf/
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -no-global-types -target bpfel -cc clang -cflags ${CFLAGS} -type event -type prefix_key opensnoopWithFullPath ./bpf/opensnoop.bpf.c -- -DWITH_FULL_PATH -I./bpf/
 
 const (
 	// Keep in sync with opensnoop.h.
@@ -101,7 +102,14 @@ func (t *Tracer) close() {
 }
 
 func (t *Tracer) install() error {
-	spec, err := loadOpensnoop()
+	var spec *ebpf.CollectionSpec
+	var err error
+
+	if t.config.FullPath {
+		spec, err = loadOpensnoopWithFullPath()
+	} else {
+		spec, err = loadOpensnoop()
+	}
 	if err != nil {
 		return fmt.Errorf("loading ebpf program: %w", err)
 	}
@@ -113,7 +121,6 @@ func (t *Tracer) install() error {
 	}
 
 	consts := make(map[string]interface{})
-	consts["get_full_path"] = t.config.FullPath
 	consts["prefixes_nr"] = prefixesNumber
 
 	for _, prefix := range t.config.Prefixes {
@@ -139,7 +146,7 @@ func (t *Tracer) install() error {
 		return fmt.Errorf("loading ebpf spec: %w", err)
 	}
 
-	// arm64 does not defined an open() syscall, only openat().
+	// arm64 does not define the open() syscall, only openat().
 	if runtime.GOARCH != "arm64" {
 		openEnter, err := link.Tracepoint("syscalls", "sys_enter_open", t.objs.IgOpenE, nil)
 		if err != nil {
@@ -229,7 +236,11 @@ func (t *Tracer) run() {
 			ModeRaw:       mode,
 			Mode:          mode.String(),
 			Path:          gadgets.FromCString(bpfEvent.Fname[:]),
-			FullPath:      gadgets.FromCString(bpfEvent.FullFname[:]),
+		}
+
+		if t.config.FullPath {
+			bpfEventWithFullPath:= (*opensnoopWithFullPathEvent)(unsafe.Pointer(&record.RawSample[0]))
+			event.FullPath = gadgets.FromCString(bpfEventWithFullPath.FullFname[:])
 		}
 
 		if t.enricher != nil {