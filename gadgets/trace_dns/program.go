17121b54e5c0f94206e5f1a5b518fbca72d8bcb6
Alban Crequy <albancrequy@linux.microsoft.com>
2023-11-01 17:56:25 +0100

    gadgets/trace_dns: add Init function in wasm module
    Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

--- gadgets/trace_dns/program.go
+++ gadgets/trace_dns/program.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"fmt"
+
 	wapc "github.com/wapc/wapc-guest-tinygo"
 )
 
 func main() {
-	wapc.RegisterFunctions(wapc.Functions{})
+	wapc.RegisterFunctions(wapc.Functions{
+		"Init":        Init,
+		"column_name": column_name,
+	})
+}
+
+func Init(payload []byte) ([]byte, error) {
+	wapc.ConsoleLog("Hello from Wasm!")
+	return nil, nil
+}
+
+func column_name(payload []byte) ([]byte, error) {
+	//wapc.ConsoleLog(fmt.Sprintf("column_name called with %q\n", string(payload)))
+	var str string
+	for i := 0; i < len(payload); i++ {
+		length := int(payload[i])
+		if length == 0 {
+			break
+		}
+		if i+1+length < len(payload) {
+			str += string(payload[i+1:i+1+length]) + "."
+		} else {
+			wapc.ConsoleLog(fmt.Sprintf("invalid payload %+v\n", payload))
+		}
+		i += length
+	}
+	//wapc.ConsoleLog(fmt.Sprintf("column_name returns %q\n", string(str)))
+	return []byte(str), nil
 }
